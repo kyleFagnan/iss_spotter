e1be193a216463d32864efa2df35983a58c1c676
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-07 19:24:42 -0700

    fetch coordinates function

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('50.65.166.29', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
